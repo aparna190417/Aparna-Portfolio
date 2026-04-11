e3f952a2378fb4df4389d09ae098429f66019d31
Aparna Patel <appuappi190417@gmail.com>
2026-04-11 15:12:26 +0530

    Final portfolio update

--- app.py
+++ app.py
@@ -7,10 +7,17 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-# Agar aapne certificates ke liye alag page rakha hai
+@app.route("/projects")
+def projects():
+    return render_template("python_projects.html")
+
 @app.route("/certificates")
 def certificates():
     return render_template("certificates.html")
+
+@app.route("/internship")
+def internship():
+    return render_template("internship.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
